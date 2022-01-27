76dfc70cb703a818aa908a3081893e23026be0e7
Pedro Polanco <59771630+polancop3@users.noreply.github.com>
2022-01-27 15:35:40 -0500

    Selection sort

---
+++ selectionSort.js
@@ -0,0 +1,22 @@
+const array = [-1,3,4,1,5,10];
+
+function selectionSort(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        let lowest = i;
+        
+        for(let j = i+1; j < arr.length; j++) {
+            if(arr[j] < arr[lowest]) {
+                lowest = j;
+            }
+        }
+        // if (lowest) no need to swap
+        if(i !== lowest) {
+            let temp = arr[i];
+            arr[i] = arr[lowest];
+            arr[lowest] = temp;
+        }
+    }
+}
+
+
+selectionSort(array)
