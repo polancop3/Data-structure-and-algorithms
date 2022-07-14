f6c713e8eaaef493f4824808d2e29f2044fe1d81
Pedro Polanco <pedropolanco02@gmail.com>
2022-07-14 10:13:09 -0400

    graph formatting fixed

--- Graphs.js
+++ Graphs.js
@@ -4,24 +4,24 @@ class Graph {
     }
 
     addVertex(vertex) {
-       if(!this.adjecencyList[vertex]) this.adjecencyList[vertex] = []
+       if(!this.adjecencyList[vertex]) this.adjecencyList[vertex] = [];
     }
 
     addEdge(v1,v2) {
-        if(this.adjecencyList[v1]) this.adjecencyList[v1].push(v2)
-        if(this.adjecencyList[v2]) this.adjecencyList[v2].push(v1)
+        if(this.adjecencyList[v1]) this.adjecencyList[v1].push(v2);
+        if(this.adjecencyList[v2]) this.adjecencyList[v2].push(v1);
     }
     
     removeEdge(v1,v2) {
-        this.adjecencyList[v1] = this.adjecencyList[v1].filter(v => v !== v2)
-        this.adjecencyList[v2] = this.adjecencyList[v2].filter(v => v !== v1)
+        this.adjecencyList[v1] = this.adjecencyList[v1].filter(v => v !== v2);
+        this.adjecencyList[v2] = this.adjecencyList[v2].filter(v => v !== v1);
 
     }
     
     removeVertex(vertex) {
         while(this.adjecencyList[vertex].length) {
            const adjecencyVertex = this.adjecencyList[vertex].pop();
-            this.removeEdge(vertex, adjecencyVertex)
+            this.removeEdge(vertex, adjecencyVertex);
         }
         delete this.adjecencyList[vertex];
     }
